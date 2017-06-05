3adec114e3d7b71095b0a0a1607005bcf442a269
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-06-05 18:34:11 -0500

    api call to retrieve legislator info

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 var geocodeURL = 'https://maps.googleapis.com/maps/api/geocode/json';
 var geocodeKey = 'AIzaSyBomPH1s3ajX87qrX0YeRJ6iiaVuGbVwpU';
+var openStatesURL = 'https://openstates.org/api/v1/legislators/geo/';
+var openStatesKey = 'ad0a422f-9bf4-4eb9-808a-0d1bb03d0867';
 
 var state = {
 results: []
 };
 
-var errorHandler = function(err){
+function errorHandler(err){
   console.error(err);
 }
 
-var successHandler = function(data){
-	state.results = data.results;
+function geocodeSuccessHandler(data){
+	var lat = data.results[0].geometry.location.lat;
+  var lng = data.results[0].geometry.location.lng;
+	getLegislatorData(lat, lng, openStatesSuccessHandler, errorHandler);
+
+}
+
+
+function openStatesSuccessHandler(data){
+  state.results = data.results;
   console.log(data);
 }
 
-function getDataFromGeocodeApi(address, successHandler, errorHandler) {
+function getDataFromGeocodeApi(address, geocodeSuccessHandler, errorHandler) {
   var settings = {
     url: geocodeURL,
     data: {
@@ -24,7 +34,7 @@ function getDataFromGeocodeApi(address, successHandler, errorHandler) {
     dataType: 'json',
     type: 'GET',
     success: function(data){
-      successHandler(data);
+      geocodeSuccessHandler(data);
     },
     error: function(err){
       errorHandler(err);
@@ -32,3 +42,24 @@ function getDataFromGeocodeApi(address, successHandler, errorHandler) {
   };
   return $.ajax(settings);
 }
+
+function getLegislatorData(lat, long, openStatesSuccessHandler, errorHandler) {
+  var settings = {
+    url: openStatesURL,
+    data: {
+      apikey: openStatesKey,
+      lat: lat,
+      long: long,
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: function(data){
+      openStatesSuccessHandler(data);
+    },
+    error: function(err){
+      errorHandler(err);
+    }
+  };
+  return $.ajax(settings);
+}
+getDataFromGeocodeApi('745 tiffany lane waconia mn', geocodeSuccessHandler, errorHandler);
